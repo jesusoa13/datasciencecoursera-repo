aa37ec9287b000241bdc683bd8859ef9d6c8f6ef
jesusoa13 <jesus.ortizagosto@gmail.com>
2021-11-21 00:21:14 +0000

    Removing personal information

--- Coursera Project.R
+++ Coursera Project.R
@@ -1,4 +1,4 @@
 "Great Course"
 "Line 3 and 4 where used to configure the repository at GitHub. They must be enter on the Terminal for it to work"
-git config --global user.name "jesusoa13"
-git config --global user.email "jesus.ortizagosto@gmail.com"
+git config --global user.name "username"
+git config --global user.email "@gmail.com"
